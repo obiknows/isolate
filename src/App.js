af782a6690df1de44adbf241e26f7fab2fe0d133
obiknows <sam.nnodim@gmail.com>
2017-12-21 21:06:51 -0800

    camera with working buttons

--- src/App.js
+++ src/App.js
@@ -23,7 +23,30 @@ export class CameraExample extends React.Component {
       return (
         <View style={{ flex: 1 }}>
           <Camera  style={{ flex: 1}} type={this.state.type}>
-            
+            <View style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row'
+            }}>
+            <TouchableOpacity 
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center'
+              }}
+              
+              onPress={() => {
+                this.setState({
+                  type: this.state.type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back,
+                });
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white'}}>
+                {'  '}Flip{' '}
+              </Text>
+            </TouchableOpacity>
+            </View>
           </Camera>
         </View>
       );
